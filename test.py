8bcea6e8d549acec515b0f6abc0475dc5f5ea17c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-04 14:09:53 +0100

    Documentation

--- test.py
+++ test.py
@@ -1,38 +1,24 @@
-import types
+import inspect
 
 
-def create_function(name, args):
-    def y(a, b, c):
-        pass
-
-    co = y.__code__
-    y_code = types.CodeType(
-        args,
-        co.co_posonlyargcount,
-        co.co_kwonlyargcount,
-        co.co_nlocals,
-        co.co_stacksize,
-        co.co_flags,
-        co.co_code,
-        co.co_consts,
-        co.co_names,
-        co.co_varnames,
-        co.co_filename,
-        co.co_name,
-        co.co_firstlineno,
-        co.co_lnotab,
-        co.co_freevars,
-        co.co_cellvars,
-    )
-
-    print(co.co_varnames)
-    d = types.FunctionType(y_code, y.__globals__, "sss")
-
-    return d
+def g(**wargs):
+    pass
 
 
-myfunc = create_function("myfunc", 3)
+def args(frame):
+    func = frame.f_globals[frame.f_code.co_name]
+    user_args = inspect.getargvalues(frame)
+    code_args = inspect.getfullargspec(func)
+    given = {}
+    for name, value in zip(code_args.args, code_args.defaults):
+        if user_args.locals[name] is not value:
+            given[name] = user_args.locals[name]
+    return given
 
-print(repr(myfunc))
 
-myfunc()
+def f(a=1, b=2, c=3):
+    print(args(inspect.currentframe()))
+
+
+
+f(b=42)
