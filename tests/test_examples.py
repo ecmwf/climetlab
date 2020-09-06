2ae2d50ac509949e375567b949228fcf5b3a1265
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-06 12:27:25 +0100

    Documentation

---
+++ tests/test_examples.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import os
+import re
+import pytest
+
+IGNORE=['conf.py', 'xml2rst.py']
+EXAMPLES = os.path.join(os.path.dirname(os.path.dirname(__file__)), "docs")
+
+
+def example_list():
+
+    # return []
+
+    examples = []
+    for root, _, files in os.walk(EXAMPLES):
+        for file in files:
+            path = os.path.join(root, file)
+            if path.endswith(".py") and  file not in IGNORE:
+                examples.append(path[len(EXAMPLES) + 1 :])
+
+    return sorted(examples)
+
+
+@pytest.mark.parametrize("path", example_list())
+def test_example(path):
+
+    with open(os.path.join(EXAMPLES, path)) as f:
+        exec(f.read())
