07cc0c4ff0744364d0764bd2c4b3fa4a6042a3b7
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-08 08:29:35 +0100

    Plotting

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
-import pandas as pd
-import numpy as np
-import xarray as xr
+#!/usr/bin/env python3
+import climetlab as cml
+from climetlab.core.bbox import BoundingBox
+import Magics
+from Magics import macro
 
-data = np.random.rand(4, 3)
+bbox = BoundingBox(north=90, west=0, east=360, south=-90)
 
-locs = ["IA", "IL", "IN"]
-
-times = pd.date_range("2000-01-01", periods=4)
-
-foo = xr.DataArray(data, coords=[times, locs], dims=["time", "space"])
-
-foo.attrs["_climetlab"] = 2
-print(foo)
-
-bar = foo.to_dataset(name="foo")
-bar.attrs["_climetlab"] = 3
-
-print(bar)
+cml.plot_map(bounding_box=bbox, projection="polar-north", path="x.png")
