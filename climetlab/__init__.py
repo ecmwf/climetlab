78a4c98f35a49652f43145276be02fa9b4e0c31d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-10-04 16:31:50 +0100

    93

--- climetlab/__init__.py
+++ climetlab/__init__.py
@@ -24,7 +24,7 @@ from .sources import load as load_source
 
 # import logging
 
-__version__ = "0.0.92"
+__version__ = "0.0.93"
 
 
 # if ipython_active:
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
 from climetlab import load_source, plot_map
 
 s = load_source("file", "docs/examples/test.grib")
-plot_map(s[0])
-plot_map(s[1])
+plot_map(s[0], path='x.png')
+plot_map(s[1], path='y.png')
 # plot_map(s)
