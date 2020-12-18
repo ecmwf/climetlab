ab5e591347724236d650b4d3fcc1cf583427484d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-12-18 18:55:14 +0000

    Patterns

---
+++ climetlab/sources/file_pattern.py
@@ -0,0 +1,24 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from . import MultiSource, File
+from climetlab.utils.pattern import Pattern
+
+
+class FilePattern(MultiSource):
+    def __init__(self, pattern, *args, **kwargs):
+        files = Pattern(pattern).substitute(*args, **kwargs)
+        if not isinstance(files, list):
+            files = [files]
+
+        sources = [File(file) for file in files]
+        super().__init__(sources)
+
+
+source = FilePattern
---
+++ climetlab/sources/url_pattern.py
@@ -0,0 +1,25 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from .multi import MultiSource
+from .url import Url
+from climetlab.utils.patterns import Pattern
+
+
+class UrlPattern(MultiSource):
+    def __init__(self, pattern, *args, **kwargs):
+        urls = Pattern(pattern).substitute(*args, **kwargs)
+        if not isinstance(urls, list):
+            urls = [urls]
+
+        sources = [Url(url) for url in urls]
+        super().__init__(sources)
+
+
+source = UrlPattern
