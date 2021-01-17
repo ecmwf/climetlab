455e27f5e3dd33fcd8c55b2de3873ff4a618370d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-01-17 16:34:53 +0000

    Just ubuntu

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-from climetlab import load_source, plot_map
+from climetlab import dataset
 
-s = load_source("file", "docs/examples/test.grib")
-plot_map(s[0], path="x.png")
-plot_map(s[1], path="y.png")
-# plot_map(s)
+P = (
+    "10m_u_component_of_wind",
+    "10m_v_component_of_wind",
+    "2m_temperature",
+    # "constants",
+    # "geopotential" ,
+    "geopotential_500",
+    "potential_vorticity",
+    "relative_humidity",
+    "specific_humidity",
+    "temperature",
+    "temperature_850",
+    "toa_incident_solar_radiation",
+    "total_cloud_cover",
+    "total_precipitation",
+    "u_component_of_wind",
+    "v_component_of_wind",
+    "vorticity",
+)
+
+R = (1.40625, 2.8125, 5.625)
+R = (5.625,)
+
+
+# for p in P:
+#     for r in R:
+#         print(p, r)
+#         ds = load_dataset("weather-bench", p, r)
+
+
+print(dataset.sample_bufr_data)
+print(help(dataset("weather-bench")))
