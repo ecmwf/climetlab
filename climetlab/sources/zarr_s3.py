9b743a3a0be20388e4d14ea3685aeb3228be8e61
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-02-09 16:50:46 +0000

    updated zarr source to merge dataset

--- climetlab/sources/zarr_s3.py
+++ climetlab/sources/zarr_s3.py
@@ -21,15 +21,27 @@ class ZarrS3(DataSource):
     def __init__(self, url, root, **kwargs):
 
         super().__init__(**kwargs)
-
+        print(url)
         fs = s3fs.S3FileSystem(anon=True, client_kwargs={"endpoint_url": url})
 
-        store = s3fs.S3Map(
-            root=root,
-            s3=fs,
-            check=False,
-        )
-        self._ds = xr.open_zarr(store)
+        if not isinstance(root,list):
+            rootlist = [root]
+        else:
+            rootlist = root
+
+        dslist = []
+        for root in rootlist:
+            print(root)
+            store = s3fs.S3Map(
+                root=root,
+                s3=fs,
+                check=False,
+            )
+            ds = xr.open_zarr(store)
+            dslist.append(ds)
+        ds = xr.merge(dslist)
+
+        self._ds = ds
 
     def to_xarray(self):
         return self._ds
