8d318b34b9e414eb1d065219985541c5a4b616d4
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-03-02 11:27:17 +0000

    chardet.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ else:
 
 install_requires += [
     # need to install  to avoid conflict between aiohttp (dependency of s3fs) and requests (cdsapi)
-    "chardet<4>=3.0",
+    "chardet>=3.0,<4.0",
     "numpy",
     "xarray>=0.17.0",
     "netcdf4",
