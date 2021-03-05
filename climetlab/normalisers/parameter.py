778ee7c59a57fe1fd6821bc434aef61e5a9656d9
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-03-05 12:56:53 +0000

    refactored parameter convention renamer

--- climetlab/normalisers/parameter.py
+++ climetlab/normalisers/parameter.py
@@ -7,74 +7,12 @@
 # nor does it submit to any jurisdiction.
 #
 
-# from climetlab.utils.parameters import yaml_loader_from_cfgrib_TODO
 import os
 import yaml
+import re
+import logging
 
-# word2id = {
-#'temperature' : 'temp_id123'
-#'2t' : 'temp_id123'
-#'t2m' : 'temp_id123'
-# }
-#
-# CONVENTIONS = {}
-# CONVENTIONS['mars'] = {
-#    'temp_id123' : '2t'
-# }
-
-# word2id = {
-#'temperature' : 'temp_id123'
-#'2t' : 'temp_id123'
-#'t2m' : 'temp_id123'
-# }
-#
-# CONVENTIONS = {}
-# CONVENTIONS['mars'] = {
-#    'temp_id123' : '2t',
-#    'temp_id45' : 'tp'
-# }
-# CONVENTIONS['cf'] = {
-#    'temp_id123' : 't2m',
-#    'temp_id45' : 'tp'
-# }
-
-ALIASES = None
-CONVENTIONS = None
-
-
-def get_alias_and_conventions(
-    yaml_file=os.path.join(os.path.dirname(__file__), "parameter.yaml")
-):
-    """ fill the global variable from the relevant yaml file """
-    global ALIASES
-    global CONVENTIONS
-    if ALIASES is None and CONVENTIONS is None:
-        path = yaml_file
-        with open(path) as f:
-            mappings = yaml.load(f.read(), Loader=yaml.SafeLoader)
-
-        def split_mapping(key):
-            sep = ":"
-            import re
-
-            if sep in key:
-                convention, rest = re.match(
-                    f"([^{sep}]*){sep}([^{sep}]*)", key
-                ).groups()
-                return convention, rest
-            else:
-                return None, key
-
-        CONVENTIONS = {"mars": {}, "cf": {}}
-        ALIASES = {}
-        for i, m in enumerate(mappings):
-            for conv_key in m:
-                convention, key = split_mapping(conv_key)
-                if convention:
-                    CONVENTIONS[convention][i] = key
-                ALIASES[key] = i
-
-    return ALIASES, CONVENTIONS
+from climetlab.utils.conventions import normalise_string
 
 
 class ParameterNormaliser:
@@ -83,15 +21,6 @@ class ParameterNormaliser:
 
     def normalise(self, parameter):
         if isinstance(parameter, (list, tuple)):
-            return [self.normalise_param(p) for p in parameter]
+            return [normalise_string(p, convention=self.convention) for p in parameter]
         else:
-            return self.normalise_param(parameter)
-
-        return parameter
-
-    def normalise_param(self, key):
-        convention = self.convention
-        ALIASES, CONVENTIONS = get_alias_and_conventions()
-        i = ALIASES.get(key, key)
-        c = CONVENTIONS[convention]
-        return c.get(i, key)
+            return normalise_string(parameter, convention=self.convention)
