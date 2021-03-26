81e03009b6165a63ab7643e5a2274ecee1075dde
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-03-26 07:48:26 +0000

    update

--- climetlab/__init__.py
+++ climetlab/__init__.py
@@ -33,7 +33,7 @@ from .plotting import options as plotting_options
 from .plotting import plot_map
 from .sources import load as load_source
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 __all__ = [
