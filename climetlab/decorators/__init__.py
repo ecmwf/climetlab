c0913dc8e352da24c2b38557632c6ed7987d79bd
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-04-01 12:33:49 +0100

    WIP

--- climetlab/decorators/__init__.py
+++ climetlab/decorators/__init__.py
@@ -7,56 +7,7 @@
 # nor does it submit to any jurisdiction.
 #
 
-import inspect
 import threading
-
-# from climetlab.normalisers import NORMALISERS
-
-
-# class parameters:
-#     def __init__(self, **kwargs):
-#         self.types = dict()
-#         for k, v in kwargs.items():
-#             if isinstance(v, str):
-#                 v = v.split(":")
-#             if hasattr(v, "normalise"):
-#                 self.types[k] = v
-#             elif hasattr(v[0], "normalise"):
-#                 assert len(v) == 1, v
-#                 self.types[k] = v[0]
-#             else:
-#                 self.types[k] = NORMALISERS[v[0]](*v[1:])
-
-#     def __call__(self, func):
-
-#         spec = inspect.getfullargspec(func)
-
-#         def wrapped(*args, **kwargs):
-
-#             request = dict()
-#             request.update(kwargs)
-
-#             for p, a in zip(spec.args, args):
-#                 request[p] = a
-
-#             request = self.normalise(request)
-#             return func(**request)
-
-#         wrapped.__name__ = func.__name__
-
-#         return wrapped
-
-#     def normalise(self, request):
-#         result = dict(**request)
-
-#         for k, v in self.types.items():
-#             if k in request:
-#                 n = v.normalise(request[k])
-#                 if n is not None:
-#                     result[k] = n
-
-#         return result
-
 
 def dict_args(func):
     def wrapped(*args, **kwargs):
