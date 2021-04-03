d9e4f849bcfd535803047733b045e3e4d8ae1928
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-04-03 11:03:54 +0100

    Add factorise

---
+++ climetlab/utils/factorise.py
@@ -0,0 +1,615 @@
+#!/usr/bin/env python
+#
+# (C) Copyright 2012-2013 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
+#
+
+
+import datetime
+import sys
+from collections import defaultdict
+from copy import copy, deepcopy
+from functools import cmp_to_key
+
+from dateutil.parser import parse as parse_dates
+
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        if isinstance(start, datetime.datetime):
+            self.one = datetime.timedelta(hours=1)
+        else:
+            self.one = datetime.timedelta(days=1)
+        assert start <= end
+
+    def split(self, dates):
+        result = []
+
+        bounds = set(dates)
+
+        bounds.remove(self.start)
+        bounds.remove(self.end)
+
+        s = self.start
+        e = self.end
+        for b in sorted(bounds):
+            if b >= s and b <= e:
+                result.append(self.__class__(s, b))
+                s = b + self.one
+
+        result.append(self.__class__(s, e))
+        return result
+
+    def overlaps(self, other):
+        s1, e1 = self.start, self.end + self.one
+        s2, e2 = other.start, other.end + self.one
+        s = max(s1, s2)
+        e = min(e1, e2)
+        if s <= e:
+            return self.__class__(min(s1, s2), max(e1, e2) - self.one)
+        else:
+            return None
+
+    @classmethod
+    def join(cls, intervals):
+        result = list(intervals)
+
+        more = True
+        while more:
+            more = False
+
+            for i in range(0, len(result)):
+                if result[i] is not None:
+                    for j in range(0, len(result)):
+                        if result[j] is not None:
+                            if i != j:
+                                if result[i].overlaps(result[j]):
+                                    result[i] = result[i].overlaps(result[j])
+                                    result[j] = None
+                                    more = True
+
+        return [r for r in result if r is not None]
+
+    def __repr__(self):
+        if isinstance(self.start, datetime.date):
+            return "%s/%s" % (
+                self.start.strftime("%Y-%m-%d"),
+                self.end.strftime("%Y-%m-%d"),
+            )
+        return "%s/%s" % (self.start.isoformat(), self.end.isoformat())
+
+    def __lt__(self, other):
+        return (self.start, self.end) < (other.start, other.end)
+
+    def __le__(self, other):
+        return (self.start, self.end) <= (other.start, other.end)
+
+    def __gt__(self, other):
+        return (self.start, self.end) > (other.start, other.end)
+
+    def __ge__(self, other):
+        return (self.start, self.end) >= (other.start, other.end)
+
+    def __eq__(self, other):
+        return (self.start, self.end) == (other.start, other.end)
+
+    def __ne__(self, other):
+        return (self.start, self.end) != (other.start, other.end)
+
+    def __hash__(self):
+        return hash((self.start, self.end))
+
+
+def _cleanup(x):
+
+    if isinstance(x, (list, tuple)):
+        return [_cleanup(a) for a in x]
+
+    if isinstance(x, dict):
+        r = {}
+        for k, v in x.items():
+            r[_cleanup(k)] = _cleanup(v)
+        return r
+
+    if isinstance(x, (str, int, float)):
+        return x
+
+    return str(repr(x))
+
+
+def _as_tuple(t):
+    if isinstance(t, tuple):
+        return t
+    if isinstance(t, list) and len(t) == 1:
+        return _as_tuple(t[0])
+    if isinstance(t, (list, set)):
+        return tuple(t)
+    return (t,)
+
+
+CODE = {}
+DECODE = []
+
+
+def encode(x):
+
+    x = _as_tuple(x)
+
+    y = CODE.get(x)
+    if y is not None:
+        return y
+
+    c = len(DECODE)
+    CODE[x] = c
+    DECODE.append(x)
+    return c
+
+
+def decode(x):
+    return DECODE[x]
+
+
+def compress_lists(lst):
+    """
+    Find common groups in list and transform them in element
+
+    => [ [1, 2, 3, 4, 5], [2, 3, 8, 9, 4], [3, 4, 5, 9, 2, 8, 1] ]
+    <= [{(1, 5), (2, 3, 4)}, {(8, 9), (2, 3, 4)}, {(8, 9), (1, 5), (2, 3, 4)}]
+
+
+    """
+
+    assert isinstance(lst, list)
+    for x in lst:
+        assert isinstance(x, list)
+
+    lst = [set(x) for x in lst]
+
+    r = list([set() for x in lst])
+
+    more = True
+    while more:
+        more = False
+        for i in range(0, len(lst)):
+            if len(lst[i]):
+                g = set(lst[i]).intersection(*lst[i + 1 :])
+                if len(g):
+                    for i, s in enumerate(lst):
+
+                        if len(s) and g <= s:
+                            r[i].add(encode(g))
+                            lst[i] = s - g
+                            more = True
+                else:
+                    r[i].add(encode(lst[i]))
+                    lst[i] = []
+
+    return [tuple(x) for x in r]
+
+
+def all_keys(requests):
+    return set().union(*[r.keys() for r in requests])
+
+
+def all_values(requests, name):
+    return [r.get(name, ["-"]) for r in requests]
+
+
+class Column(object):
+    """Just what is says on the tin, a column of values."""
+
+    def __init__(self, title, values):
+        self.title = title
+        self.values = values
+        self.prio = 0
+        self.diff = -1
+
+    def __lt__(self, other):
+        return (self.prio, self.diff, self.title) < (
+            other.prio,
+            other.diff,
+            other.title,
+        )
+
+    def value(self, i):
+        return self.values[i]
+
+    def set_value(self, i, v):
+        self.values[i] = v
+
+    def compute_differences(self, idx):
+        """
+        Number of unique values in this column for the requested
+        row indexes.
+
+        @param idx list of row indexes
+        """
+        x = [self.values[i] for i in idx]
+        try:
+            self.diff = len(set(x))
+        except Exception:
+            print(type(x))
+            print(x[:10])
+            raise
+
+    def __repr__(self):
+        return "Column(%s,%s,%s,%s)" % (self.title, self.values, self.prio, self.diff)
+
+
+class Table(object):
+    def __init__(self, other=None, a=None, b=None):
+
+        self.tree = {"_kids": []}
+
+        if other is not None:
+            self.depth = other.depth + 1
+            self.cols = copy(other.cols)
+            self.colidx = copy(other.colidx)
+            self.rowidx = other.rowidx[a:b]
+        else:
+            self.depth = 0
+            self.cols = []
+            self.colidx = []
+            self.rowidx = []
+
+    def get_elem(self, c, r):
+        return self.cols[self.colidx[c]].value(self.rowidx[r])
+
+    def set_elem(self, c, r, v):
+        return self.cols[self.colidx[c]].set_value(self.rowidx[r], v)
+
+    def __repr__(self):
+        return repr(
+            [[self.cols[col].value(row) for row in self.rowidx] for col in self.colidx]
+        )
+
+    def column(self, s, col):
+        self.cols.append(Column(s, col))
+        self.colidx.append(len(self.colidx))
+
+        if len(col) > len(self.rowidx):
+            self.rowidx = [i for i in range(0, len(col))]
+
+    def one_less(self, r, n):
+        return [self.get_elem(i, r) for i in range(len(self.colidx)) if i != n]
+
+    def factorise1(self):
+        self.pop_singles()
+        self.sort_rows()
+
+        for i in range(0, len(self.colidx)):
+            self.factorise2(len(self.colidx) - i - 1)
+
+    def factorise2(self, n):
+        remap = defaultdict(list)
+        gone = []
+
+        for i in range(0, len(self.rowidx)):
+            v = self.one_less(i, n)
+            s = remap[_as_tuple(v)]
+            if len(s) != 0:
+                gone.append(i)
+            elem = self.get_elem(n, i)
+            if elem not in s:
+                s.append(elem)
+
+        for g in reversed(gone):
+            del self.rowidx[g]
+
+        for i in range(0, len(self.rowidx)):
+            v = self.one_less(i, n)
+            s = remap[_as_tuple(v)]
+            self.set_elem(n, i, _as_tuple(s))
+
+    def sort_columns(self):
+        """
+        Sort the columns on the number of unique values (this column.diff).
+        """
+        for idx in self.colidx:
+            self.cols[idx].compute_differences(self.rowidx)
+
+        self.colidx.sort(key=lambda a: self.cols[a])
+
+    def compare_rows(self, a, b):
+        for idx in self.colidx:
+            sa = self.cols[idx].value(a)
+            sb = self.cols[idx].value(b)
+
+            if sa is None and sb is None:
+                continue
+
+            if sa < sb:
+                return -1
+
+            if sa > sb:
+                return 1
+
+        return 0
+
+    def sort_rows(self):
+        self.rowidx.sort(key=cmp_to_key(self.compare_rows))
+
+    def pop_singles(self):
+        """
+        Take the column with just one unique value and add them to the
+        tree. Delete their index from the list of column indexes.
+        """
+        self.sort_columns()
+        ok = False
+        while len(self.colidx) > 0 and self.cols[self.colidx[0]].diff == 1:
+            s = _as_tuple(self.get_elem(0, 0))
+            self.tree[self.cols[self.colidx[0]].title] = s
+            del self.colidx[0]
+            ok = True
+
+        return ok
+
+    def split(self):
+        if len(self.rowidx) < 2 or len(self.colidx) < 2:
+            return
+
+        self.sort_columns()
+        self.sort_rows()
+
+        prev = self.get_elem(0, 0)
+        j = 0
+
+        for i in range(1, len(self.rowidx)):
+            e = self.get_elem(0, i)
+            if prev != e:
+                table = Table(self, j, i)
+                self.tree["_kids"].append(table.process())
+                j = i
+                prev = e
+
+        if j > 0:
+            table = Table(self, j, len(self.rowidx))
+            self.tree["_kids"].append(table.process())
+            self.rowidx = []
+
+    def process(self):
+        self.factorise1()
+        self.pop_singles()
+        self.split()
+        return self.tree
+
+
+def scan(r, cols, name, rest):
+    """Generate all possible combinations of values. Each set of values is
+    stored in a list and each value is repeated as many times so that if taking
+    one row in all value lists, I will get one unique combination of values
+    between all input keys.
+
+    @param r    request as a dict
+    @param cols actual result of the scan
+    @param name current request key we are processing
+    @param rest remaining request keys to process
+    """
+    n = 0
+    # print("Scan", name)
+    c = cols[name]
+    for value in r.get(name, ["-"]):
+        m = 1
+        if rest:
+            m = scan(r, cols, rest[0], rest[1:])
+        for _ in range(0, m):
+            c.append(value)
+        n += m
+
+    return n
+
+
+def as_requests(r):
+
+    s = {}
+    for k, v in r.items():
+        if not isinstance(v, (tuple, list)):
+            s[k] = [v]
+        else:
+            s[k] = v
+
+    return s
+
+
+def encode_requests(req):
+    if not isinstance(req, list):
+        req = [req]
+
+    for r in req:
+        for k, v in r.items():
+            if not isinstance(v, (list, set, tuple)):
+                r[k] = [v]
+
+    for name in all_keys(req):
+        p = all_values(req, name)
+        for r, l in zip(req, compress_lists(p)):
+            r[name] = l
+
+
+def _join_intervals(what, req):
+    for r in req:
+        if "_kids" in r:
+            _join_intervals(what, r["_kids"])
+
+        if what in r:
+            r[what] = Interval.join(r[what])
+
+
+def decode_tree(req):
+    for r in req:
+        if "_kids" in r:
+            decode_tree(r["_kids"])
+
+        for k, v in r.items():
+            if k != "_kids":
+                newv = []
+                for x in v:
+                    newv.extend(decode(x))
+                r[k] = newv
+
+
+def factorise(req, compress=False, intervals=[]):
+    return _factorise(deepcopy(req), compress, intervals)
+
+
+def _factorise(req, compress=False, intervals=[]):
+
+    # print("factorise compress %s %s %s" % (compress, len(req), intervals))
+
+    if intervals:
+        for r in req:
+            for i in intervals:
+                if i in r:
+                    r[i] = _to_interval(r[i])
+
+    for i in intervals:
+        # Collect all dates
+        dates = set()
+        for r in req:
+            for interval in r.get(i, []):
+                dates.add(interval.start)
+                dates.add(interval.end)
+
+        # Split intervals according to collected dates
+        for r in req:
+            if i in r:
+                splits = []
+                for interval in r.get(i, []):
+                    splits.extend(interval.split(dates))
+                r[i] = splits
+
+    if compress:
+        encode_requests(req)
+
+    req = [as_requests(r) for r in req]
+
+    names = list(set(name for r in req for name in r.keys()))
+    # print(names)
+
+    cols = defaultdict(list)
+    if names:
+        for r in req:
+            scan(r, cols, names[0], names[1:])
+
+    table = Table()
+    for n, c in cols.items():
+        # print(n, len(c))
+        table.column(n, c)
+
+    r = table.process()
+
+    if compress:
+        decode_tree([r])
+
+    for i in intervals:
+        _join_intervals(i, [r])
+
+    return Tree(r)
+
+
+def _flatten_tree(r):
+    """Used with the output of the factoriser to flatten the tree it produces
+    into linear requests.
+
+    Example:
+
+    >>> _flatten_tree({'a': 1, '_kids': [{'b': 2}, {'b': 3}]})
+    [{'a': 1, 'b': 2} , {'a': 1, 'b': 3}]
+    """
+    if "_kids" not in r:
+        return [r]
+
+    kids = r["_kids"]
+
+    if kids:
+        t = []
+        for k in kids:
+            for n in _flatten_tree(k):
+                s = {}
+                for k, v in r.items():
+                    s[k] = v
+                del s["_kids"]
+                s.update(n)
+                t.append(s)
+        return t
+    else:
+        s = {}
+        for k, v in r.items():
+            s[k] = v
+        del s["_kids"]
+        return [s]
+
+
+def _put_tree(r, depth=0, file=sys.stdout):
+    n = 0
+    t = []
+    for k, v in r.items():
+        if k != "_kids":
+            t.append("%s=%s" % (k, "/".join([str(x) for x in v])))
+    if len(t):
+        spc = " " * depth
+        file.write("%s%s" % (spc, ",".join(t)) + "\n")
+        n = 3
+
+    if "_kids" in r:
+        for k in r["_kids"]:
+            _put_tree(k, depth + n, file)
+
+
+class Tree:
+    def __init__(self, tree):
+        self.tree = tree
+        self._unique_values = None
+        self._flatten = None
+
+    @property
+    def unique_values(self):
+        if self._unique_values is None:
+            u = self._unique_values = {}
+
+            for r in self.flatten:
+                for k, v in r.items():
+                    u.setdefault(k, set())
+                    for x in v:
+                        u[k].add(x)
+
+        return self._unique_values
+
+    @property
+    def flatten(self):
+        if self._flatten is None:
+            self._flatten = _flatten_tree(self.tree)
+        return self._flatten
+
+    def to_list(self):
+
+        result = []
+        for r in _cleanup(self.flatten):
+            s = {}
+            for k, v in sorted(r.items()):
+                s[k] = sorted(v)
+            result.append(s)
+
+        return sorted(result, key=lambda a: sorted(a.items()))
+
+    def dump(self, file=sys.stdout):
+        _put_tree(self.tree, file=file)
+
+
+def _to_interval(interval):
+    if not isinstance(interval, (list, tuple)):
+        interval = [interval]
+    result = []
+    for t in interval:
+        start, end = t.split("/")
+        start = parse_dates(start)
+        end = parse_dates(end)
+        result.append(Interval(start, end))
+    return result
---
+++ tests/test_constraints.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from climetlab.utils.factorise import factorise
+
+C0 = [
+    {"level": "500", "param": "Z", "step": "24"},
+    {"level": "500", "param": "Z", "step": "36"},
+    {"level": "500", "param": "Z", "step": "48"},
+    {"level": "500", "param": "T", "step": "24"},
+    {"level": "500", "param": "T", "step": "36"},
+    {"level": "500", "param": "T", "step": "48"},
+    {"level": "850", "param": "T", "step": "36"},
+    {"level": "850", "param": "T", "step": "48"},
+    {"level": "1000", "param": "Z", "step": "24"},
+    {"level": "1000", "param": "Z", "step": "48"},
+]
+
+C1 = [
+    {"level": ["1000"], "param": ["Z"], "step": ["24", "48"]},
+    {"level": ["500"], "param": ["T", "Z"], "step": ["24", "36", "48"]},
+    {"level": ["850"], "param": ["T"], "step": ["36", "48"]},
+]
+
+
+def _(x):
+    """ Make list of dicts comparable """
+
+    def __(r):
+        for k, v in sorted(r.items()):
+            yield (k, tuple(sorted(v)))
+
+    return tuple(sorted(tuple(__(a)) for a in x))
+
+
+def test_factorise_1():
+    f = factorise(C0)
+    assert _(f.to_list()) == _(C1)
+
+    assert f.unique_values == {
+        "param": {"Z", "T"},
+        "level": {"500", "850", "1000"},
+        "step": {"36", "48", "24"},
+    }
+
+
+def test_factorise_2():
+    assert _(factorise(C1).to_list()) == _(C1)
+
+
+def test_factorise_3():
+
+    c = factorise(
+        [
+            {"date": ["1990-01-01/1990-01-02"]},
+            {"date": ["1990-01-02/1990-01-05"]},
+            {"date": ["1990-01-04/1990-01-15"]},
+        ],
+        intervals=["date"],
+    ).to_list()
+
+    assert _(c) == _([{"date": ["1990-01-01/1990-01-15"]}])
+
+    c = factorise(
+        [
+            {"date": ["1990-01-01/1990-01-02"]},
+            {"date": ["1990-01-04/1990-01-15"]},
+        ],
+        intervals=["date"],
+    ).to_list()
+
+    assert _(c) == _([{"date": ["1990-01-01/1990-01-02", "1990-01-04/1990-01-15"]}])
+
+    c = factorise(
+        [
+            {"date": ["1990-01-01/1990-01-10"]},
+            {"date": ["1990-01-04/1990-01-15"]},
+        ],
+        intervals=["date"],
+    ).to_list()
+
+    assert _(c) == _([{"date": ["1990-01-01/1990-01-15"]}])
+
+
+if __name__ == "__main__":
+    # test_factorise_1()
+    test_factorise_2()
