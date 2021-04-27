f676cf13dae855b6dbf5f8b280d568b4fcf83f37
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-04-27 13:32:32 +0000

    make black happy

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 
 def is_package_installed(package):
-    """ return true if all packages in "package" are installed """
+    """return true if all packages in "package" are installed"""
     if isinstance(package, (list, tuple)):
         installed = [p for p in package if is_package_installed(p)]
         if len(installed) != len(package):
