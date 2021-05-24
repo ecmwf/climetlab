b0df24af15c57b070334ff19f1226d773504caee
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-05-24 12:21:16 +0000

    parallel download ok. but num_tread set to 1 because of sql database

--- climetlab/sources/url_pattern.py
+++ climetlab/sources/url_pattern.py
@@ -7,10 +7,49 @@
 # nor does it submit to any jurisdiction.
 #
 
+from queue import Queue
+from threading import Thread
+
 from climetlab.utils.patterns import Pattern
 
 from .multi import MultiSource
 from .url import Url
+
+
+class Worker(Thread):
+    def __init__(self, tasks, sources):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.sources = sources
+        # TODO : Make sure the thread are stopped eventually
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            url = self.tasks.get()
+            try:
+                source = Url(url)
+                self.sources.append(source)
+            except Exception as e:
+                # TODO add better handling of exceptions
+                print(e)
+            finally:
+                self.tasks.task_done()
+
+
+class DownloaderPool:
+    def __init__(self, urls, num_threads=1):
+        self.urls = urls
+        self.tasks = Queue(num_threads)
+        self.sources = []
+        for t in range(num_threads):
+            Worker(self.tasks, self.sources)
+
+    def process_all(self):
+        for url in self.urls:
+            self.tasks.put(url)
+        self.tasks.join()
 
 
 class UrlPattern(MultiSource):
@@ -19,7 +58,10 @@ class UrlPattern(MultiSource):
         if not isinstance(urls, list):
             urls = [urls]
 
-        sources = [Url(url) for url in urls]
+        downloader = DownloaderPool(urls)
+        downloader.process_all()
+        sources = downloader.sources
+
         super().__init__(sources, merger=merger)
 
 
