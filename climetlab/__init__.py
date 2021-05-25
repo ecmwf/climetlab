03fea095dff9ca8bc874cd7e3e790c2e45599c6a
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-05-25 10:10:50 +0100

    New version

--- climetlab/__init__.py
+++ climetlab/__init__.py
@@ -18,7 +18,7 @@ from .plotting import plot_map
 from .readers import Reader
 from .sources import Source, load_source, register_source, source
 
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 
 __all__ = [
