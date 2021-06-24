d0201273306100238357658ce5b93b46085bafef
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-06-24 11:45:03 +0000

    split url_pattern to create multi_url

---
+++ climetlab/sources/multi_url.py
@@ -0,0 +1,40 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+try:
+    import ipywidgets  # noqa
+    from tqdm.auto import tqdm
+except ImportError:
+    from tqdm import tqdm
+
+
+from climetlab.core.thread import SoftThreadPool
+
+from .multi import MultiSource
+from .url import Url
+
+
+class MultiUrl(MultiSource):
+    def __init__(self, urls, *args, merger=None, **kwargs):
+        if not isinstance(urls, (list, tuple)):
+            urls = [urls]
+
+        nthreads = min(self.settings("number-of-download-threads"), len(urls))
+
+        if nthreads < 2:
+            sources = [Url(url) for url in urls]
+        else:
+            with SoftThreadPool(nthreads=nthreads) as pool:
+
+                futures = [pool.submit(Url, url, watcher=pool) for url in urls]
+
+                iterator = (f.result() for f in futures)
+                sources = list(tqdm(iterator, leave=True, total=len(urls)))
+
+        super().__init__(sources, merger=merger)
--- climetlab/sources/url_pattern.py
+++ climetlab/sources/url_pattern.py
@@ -7,54 +7,14 @@
 # nor does it submit to any jurisdiction.
 #
 
-try:
-    import ipywidgets  # noqa
-    from tqdm.auto import tqdm
-except ImportError:
-    from tqdm import tqdm
-
-
-from climetlab.core.thread import SoftThreadPool
+from climetlab.sources.multi_url import MultiUrl
 from climetlab.utils.patterns import Pattern
 
-from .multi import MultiSource
-from .url import Url
 
-
-class UrlPattern(MultiSource):
+class UrlPattern(MultiUrl):
     def __init__(self, pattern, *args, merger=None, **kwargs):
         urls = Pattern(pattern).substitute(*args, **kwargs)
-        if not isinstance(urls, list):
-            urls = [urls]
-
-        def url_to_source(url):  # , pbar):
-            # time.sleep(4)
-            url = Url(url)
-            #            pbar.update(1)
-            return url
-
-        nthreads = min(self.settings("number-of-download-threads"), len(urls))
-        # import os
-        # with tqdm(
-        #     total=len(urls),
-        #     disable=False,
-        #     leave=False,
-        #     # desc='Downloading'
-        # ) as pbar:
-        #     total = 0
-        #     pbar.update(total)
-
-        if nthreads < 2:
-            sources = [Url(url) for url in urls]
-        else:
-            with SoftThreadPool(nthreads=nthreads) as pool:
-
-                futures = [pool.submit(Url, url, watcher=pool) for url in urls]
-
-                iterator = (f.result() for f in futures)
-                sources = list(tqdm(iterator, leave=True, total=len(urls)))
-
-        super().__init__(sources, merger=merger)
+        super().__init__(urls, *args, merger=merger, **kwargs)
 
 
 source = UrlPattern
