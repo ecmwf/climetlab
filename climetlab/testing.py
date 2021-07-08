f6056548520e3986ccd74d57510a2fa802304e74
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-07-08 16:25:01 +0100

    Refactor tests

---
+++ climetlab/testing.py
@@ -0,0 +1,55 @@
+import logging
+import os
+import pathlib
+from importlib import import_module
+
+LOG = logging.getLogger(__name__)
+
+
+def climetlab_file(*args):
+    return os.path.join(os.path.dirname(os.path.dirname(__file__)), *args)
+
+
+def data_file(*args):
+    return os.path.join(os.path.dirname(__file__), "data", *args)
+
+
+def file_url(path):
+    return pathlib.Path(os.path.abspath(path)).as_uri()
+
+
+def data_file_url(*args):
+    return file_url(data_file(*args))
+
+
+def modules_installed(*modules):
+    for module in modules:
+        try:
+            import_module(module)
+        except ImportError:
+            return False
+    return True
+
+
+def can_call_mars():
+    return os.path.exists(os.path.expanduser("~/.ecmwfapirc"))
+
+
+def can_call_cds():
+    return os.path.exists(os.path.expanduser("~/.cdsapirc"))
+
+
+def main(globals):
+    logging.basicConfig(level=logging.DEBUG)
+    for k, f in sorted(globals.items()):
+        if k.startswith("test_") and callable(f):
+            skip = None
+            if hasattr(f, "pytestmark"):
+                for m in f.pytestmark:
+                    if m.name == "skipif" and m.args[0] is True:
+                        skip = m.kwargs.get("reason", "?")
+            if skip:
+                LOG.debug("========= Skipping '%s' %s", k, skip)
+            else:
+                LOG.debug("========= Running '%s'", k)
+                f()
---
+++ tests/readers/test_csv.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import os
+
+import pytest
+
+import climetlab as cml
+from climetlab.testing import modules_installed
+
+
+def test_csv_1():
+    s = cml.load_source(
+        "dummy-source",
+        "csv",
+        headers=["a", "b", "c"],
+        lines=[
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ],
+    )
+
+    print(s.to_pandas())
+
+
+def test_csv_2():
+    s = cml.load_source(
+        "dummy-source",
+        "csv",
+        headers=["a", "b", "c"],
+        lines=[
+            [1, None, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ],
+    )
+
+    print(s.to_pandas())
+
+
+def test_csv_3():
+    s = cml.load_source(
+        "dummy-source",
+        "csv",
+        headers=["a", "b", "c"],
+        lines=[
+            [1, "x", 3],
+            [4, "y", 6],
+            [7, "z", 9],
+        ],
+    )
+
+    print(s.to_pandas())
+
+def test_csv_4():
+    s = cml.load_source(
+        "dummy-source",
+        "csv",
+        headers=["a", "b", "c"],
+        quote_strings=True,
+        lines=[
+            [1, "x", 3],
+            [4, "y", 6],
+            [7, "z", 9],
+        ],
+    )
+
+    print(s.to_pandas())
+
+if __name__ == "__main__":
+    from climetlab.testing import main
+
+    main(globals())
