57f05306d97e3f167bb35f20290f4b1db1dce13d
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-09-01 14:37:04 +0000

    added basic "climetlab check"

--- climetlab/scripts.py
+++ climetlab/scripts.py
@@ -1,26 +1,34 @@
+import os
 import sys
+
+import climetlab
 
 
 def check():
     print("Checking climetlab installation.")
+    print(f"Climetlab installed in {os.path.dirname(climetlab.__file__)}")
     print("Checking required dependencies...")
     import xarray  # noqa: F401
 
-    print("xarray: ok")
+    print(f"xarray: ok ({os.path.dirname(xarray.__file__)})")
     import pandas  # noqa: F401
 
-    print("pandas: ok")
+    print(f"pandas: ok ({os.path.dirname(pandas.__file__)})")
     # TODO: add more
     # print('All required dependencies seems to be ok.')
 
     print("Checking optional dependencies...")
     try:
         import pdbufr  # noqa: F401
+
+        print(f"pdbufr: ok ({os.path.dirname(pdbufr.__file__)})")
     except Exception as e:
         print(e)
         print("Warning: cannot import pdbufr. Limited capabilities.")
     try:
         import pyodc  # noqa: F401
+
+        print(f"pyodc: ok ({os.path.dirname(pyodc.__file__)})")
     except Exception as e:
         print(e)
         print("Warning: cannot import pyodc. Limited capabilities.")
