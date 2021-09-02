5184d69c73661a332acd129916f5ee6ffdac2ca3
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-09-02 13:44:51 +0000

    added extra_requires to setup

--- setup.py
+++ setup.py
@@ -72,10 +72,17 @@ install_requires += [
     "jellyfish",
     "entrypoints",
     "branca==0.3.1",  # See https://github.com/python-visualization/branca/issues/81"
-    "folium>=0.12.1",
 ]
 
-extras_require = {}  # {"interactive": ["skinnywms", "folium"]}
+extras_require = {
+    "interactive": [
+        "skinnywms",
+        "folium>=0.12.1",
+    ],
+    "tensorflow": [
+        "tensorflow",
+    ],
+}
 
 
 full = []
