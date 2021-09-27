c2d473a61dd6de324a6c906a7bb1b35da3860318
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-09-27 14:10:25 +0000

    move benchmark test to benchmarks/.

--- tests/sources/test_multi.py
+++ tests/sources/test_multi.py
@@ -14,7 +14,6 @@ import logging
 import os
 
 import pytest
-import xarray as xr
 
 from climetlab import load_source
 from climetlab.core.temporary import temp_directory, temp_file
@@ -279,48 +278,6 @@ def test_large_multi_1_climetlab(benchmark):
         return ds
 
     large_multi_1(benchmark, func)
-
-
-def large_multi_2_climetlab():
-    import pandas as pd
-
-    pattern = "https://storage.ecmwf.europeanweather.cloud/MAELSTROM_AP1/{parameter}_{size}/{date}T00Z.nc"
-
-    dates = [
-        i.strftime("%Y%m%d")
-        for i in pd.date_range(start="2017-01-01", end="2017-12-31", freq="1D")
-    ][:200]
-    request = {"size": "5GB", "parameter": "air_temperature", "date": dates}
-
-    s = load_source("url-pattern", pattern, request)
-    return [t.path for t in s.sources]
-
-
-@pytest.mark.long_test
-@pytest.mark.external_download
-@pytest.mark.skipif(
-    MISSING("pytest_benchmark"), reason="pytest-benchmark not installed"
-)
-def test_large_multi_2_climetlab(benchmark):
-    with temp_directory():
-        large_multi_2_climetlab()
-        benchmark(large_multi_2_climetlab)
-
-
-def large_multi_2_xarray(paths):
-    xr.open_mfdataset(paths, combine="nested", concat_dim="record")
-
-
-@pytest.mark.long_test
-@pytest.mark.external_download
-@pytest.mark.skipif(
-    MISSING("pytest_benchmark"), reason="pytest-benchmark not installed"
-)
-def test_large_multi_2_xarray(benchmark):
-
-    with temp_directory():
-        paths = large_multi_2_climetlab()
-        benchmark(large_multi_2_xarray, paths)
 
 
 if __name__ == "__main__":
