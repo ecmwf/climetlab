5ccf78e65c63bcb7c8f75a96fe55de550341b6c4
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-10-15 11:58:14 +0000

    more test

--- tests/test_script.py
+++ tests/test_script.py
@@ -8,9 +8,12 @@
 #
 
 import logging
+import re
 
 import pytest
+import yaml
 
+from climetlab import settings
 from climetlab.scripts.main import CliMetLabApp
 
 LOG = logging.getLogger(__name__)
@@ -49,11 +52,66 @@ def test_cli_cache(capsys):
     assert err == "", err
 
 
+def test_cli_setting_1(capsys):
+    app = CliMetLabApp()
+    app.onecmd("settings --json")
+    out, err = capsys.readouterr()
+    assert err == "", err
+
+    # yaml is a superset of json. yaml.safe_load can read json
+    dic = yaml.safe_load(out)
+
+    assert len(dic) > 2
+
+    for s in settings.dump():
+        assert dic[s[0]] == s[1]
+
+
+def do_nothing(text, *args, **kwargs):
+    assert 0
+    return text
+
+
+@pytest.fixture
+def settings_dict():
+    return {s[0]: s[1] for s in settings.dump()}
+
+
+def test_cli_setting_2(capsys, settings_dict):
+    app = CliMetLabApp()
+    app.onecmd("settings")
+    out, err = capsys.readouterr()
+    assert err == "", err
+
+    lines = out.splitlines(True)
+    assert lines
+
+    for line in lines:
+        assert line
+        m = re.match("([^ ]*) *([^ ]+)", line)
+        assert m is not None, line
+        key = m.groups()[0]
+        key = key.strip()
+        value = m.groups()[1]
+        # TODO: replace the following line by stripping colorcodes or add an option --no-color
+        value = value.strip()[5:-4]
+        assert key in settings_dict
+        assert value == str(settings_dict[key])
+
+
+def test_cli_df(capsys):
+    app = CliMetLabApp()
+    app.onecmd("df")
+    out, err = capsys.readouterr()
+    assert out.startswith("DiskUsage("), out
+    assert err == "", err
+
+
 if __name__ == "__main__":
-    from climetlab.testing import main
 
     LOG.debug(
         f"Skipping {__file__} tests: must run with pytest because they use capsys fixture."
     )
 
+    # from climetlab.testing import main
     # main(globals())
