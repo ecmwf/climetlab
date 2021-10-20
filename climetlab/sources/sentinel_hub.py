167c4bd88fe6a6b04082bac528bc5c65b7bb82cb
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-10-20 08:21:35 +0000

    wip sentinelhub (see with esa).

---
+++ climetlab/sources/sentinel_hub.py
@@ -0,0 +1,36 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+# WIP.
+
+import os
+import json
+import climetlab as cml
+
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+
+# Your client credentials
+with open(os.path.expanduser("~/.sentinelhubrc")) as f:
+    credentials = json.loads(f.read())
+client_id = credentials['client_id']
+client_secret = credentials['client_secret']
+
+# Create a session
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
+
+
+# Get token for the session
+token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/oauth/token',
+                          client_id=client_id, client_secret=client_secret)
+
+# All requests using this session will have an access token automatically added
+resp = oauth.get("https://services.sentinel-hub.com/oauth/tokeninfo")
+print(resp.content)
