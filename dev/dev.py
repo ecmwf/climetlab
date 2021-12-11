536006cb79d2bc4f68eb87d244a3174962170305
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-12-11 08:36:50 +0000

    qa

--- dev/dev.py
+++ dev/dev.py
@@ -1,19 +1,25 @@
+import xarray as xr
+
+import climetlab as cml
+
+
 def build_netcdf():
     from climetlab import load_source
+
     source = load_source(
         "dummy-source",
         kind="netcdf",
         dims=["lat", "lon"],
     )
     ds = source.to_xarray()
-    ds.to_netcdf('test.nc')
+    ds.to_netcdf("test.nc")
+
+
 build_netcdf()
 
 
-import climetlab as cml
-import xarray as xr
-ds = xr.open_dataset('test.nc')
+ds = xr.open_dataset("test.nc")
 print(ds)
 
 # works on linux,  fails on windows
-cml.plot_map(ds, path='test.png')
+cml.plot_map(ds, path="test.png")
