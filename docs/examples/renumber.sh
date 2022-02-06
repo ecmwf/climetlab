17a04f3fdf2d7f1e08827f399e727f8b39f3efff
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2022-02-06 11:57:01 +0000

    renumber

---
+++ docs/examples/renumber.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for n in $(seq $1 89)
+do
+   m=$((n+1)) 
+   f1=$(printf '%02d-' $n)
+   f2=$(printf '%02d-' $m)
+   p=$(ls $f1*.ipynb 2>/dev/null)
+   if [[ -f $p ]]
+   then
+      q=$(echo $p | sed s/$f1/$f2/)
+      echo $p $q
+      git mv $p $q
+   fi
+done
