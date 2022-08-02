923c466e0ae49e8db6ef98ad27016114a357c776
Florian Pinault <Florian.Pinault@ecmwf.int>
2022-08-02 13:17:00 +0200

    up

--- climetlab/readers/grib/index.py
+++ climetlab/readers/grib/index.py
@@ -308,7 +308,7 @@ class GribFileIndex(GribIndexFromFile):
 
     def __init__(self, path):
         assert isinstance(path, str), path
-        super().__init__(path)
+        super().__init__()
         self.path = path
         self.offsets = None
         self.lengths = None
