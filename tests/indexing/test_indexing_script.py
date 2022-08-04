feb61ea85150fcabf733453ad2f40c0f9b15f3a3
Florian Pinault <Florian.Pinault@ecmwf.int>
2022-08-04 10:00:26 +0200

    up

--- tests/indexing/test_indexing_script.py
+++ tests/indexing/test_indexing_script.py
@@ -21,53 +21,50 @@ from climetlab import settings
 from climetlab.core.temporary import temp_directory
 from climetlab.testing import NO_CDS
 
+def fill_cache_with_cds():
+    cml.load_source(
+        "cds",
+        "reanalysis-era5-single-levels",
+        variable=["2t", "msl"],
+        product_type="reanalysis",
+        area=[50, -50, 20, 50],
+        date="2011-12-02",
+        time="12:00",
+    )
+
+    s = cml.load_source(
+        "cds",
+        "reanalysis-era5-single-levels",
+        variable=["2t", "msl"],
+        product_type="reanalysis",
+        area=[50, -50, 20, 50],
+        date="2008-07-19",
+        time="12:00",
+    )
+    path = s.path
+
+    assert len(s) == 2
+
+    return path
 
 @pytest.mark.skipif(NO_CDS, reason="No access to CDS")
 def test_script_export_cache_cds():
-    def fill_cache():
-        cml.load_source(
-            "cds",
-            "reanalysis-era5-single-levels",
-            variable=["2t", "msl"],
-            product_type="reanalysis",
-            area=[50, -50, 20, 50],
-            date="2011-12-02",
-            time="12:00",
-        )
-
-        s = cml.load_source(
-            "cds",
-            "reanalysis-era5-single-levels",
-            variable=["2t", "msl"],
-            product_type="reanalysis",
-            area=[50, -50, 20, 50],
-            date="2008-07-19",
-            time="12:00",
-        )
-        path = s.path
-
-        assert len(s) == 2
-
-        return path
-
     # Since we are using a system call to test the script,
     # the actual settings file is modified
-    # the value of cache-directory is set back to its original value
-    # using a try: finally: below
+    # The value of cache-directory is set back to its original value
+    # using a try/finally
     original_cachedir = settings.get("cache-directory")
-
-    with temp_directory() as cache_dir:
-        with settings.temporary():
-            settings.set("cache-directory", cache_dir)
-
-            path_in_cache = fill_cache()
-
-            with temp_directory() as export_dir:
+    with temp_directory() as export_dir:
+        with temp_directory(), temp_directory() as cache_dir:
+            with settings.temporary():
+                settings.set("cache-directory", cache_dir)
                 cmd = f"climetlab settings cache-directory {cache_dir}"
                 out = subprocess.check_output(cmd.split(" ")).decode("utf-8")
                 print(cmd, out)
 
                 try:
+                    path_in_cache = fill_cache_with_cds()
+
                     cmd = f'climetlab export_cache --match "era5" --directory {export_dir}'
                     out = subprocess.check_output(cmd.split(" ")).decode("utf-8")
                     print(cmd, out)
@@ -83,6 +80,37 @@ def test_script_export_cache_cds():
                     path_in_cache, path_in_target
                 ), f"Exported {path_in_target} differs from original file {path_in_cache}"
 
+def test_script_export_cache_cds():
+    # Since we are using a system call to test the script,
+    # the actual settings file is modified
+    # The value of cache-directory is set back to its original value
+    # using a try/finally
+    original_cachedir = settings.get("cache-directory")
+    with temp_directory() as export_dir:
+        with temp_directory(), temp_directory() as cache_dir:
+            with settings.temporary():
+                settings.set("cache-directory", cache_dir)
+                cmd = f"climetlab settings cache-directory {cache_dir}"
+                out = subprocess.check_output(cmd.split(" ")).decode("utf-8")
+                print(cmd, out)
+
+                try:
+                    path_in_cache = fill_cache()
+
+                    cmd = f'climetlab export_cache --match "era5" --directory {export_dir}'
+                    out = subprocess.check_output(cmd.split(" ")).decode("utf-8")
+                    print(cmd, out)
+                finally:
+                    cmd = f"climetlab settings cache-directory {original_cachedir}"
+                    subprocess.check_output(cmd.split(" ")).decode("utf-8")
+
+                exported_files = glob.glob(os.path.join(export_dir, "*"))
+                assert len(exported_files) == 2, exported_files
+
+                path_in_target = f"{export_dir}/{os.path.basename(path_in_cache)}"
+                assert filecmp.cmp(
+                    path_in_cache, path_in_target
+                ), f"Exported {path_in_target} differs from original file {path_in_cache}"
 
 if __name__ == "__main__":
     from climetlab.testing import main
