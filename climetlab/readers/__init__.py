d941e2c6ccc3cbd524b8b4e1bbf146ec41838059
Florian Pinault <Florian.Pinault@ecmwf.int>
2022-08-11 16:03:55 +0000

    wip

--- climetlab/readers/__init__.py
+++ climetlab/readers/__init__.py
@@ -82,6 +82,10 @@ class Reader(Base, os.PathLike, metaclass=ReaderMeta):
 
     def __fspath__(self):
         return self.path
+    
+    def index_content(self):
+        LOG.warning('index-content(): Ignoring {self.path}')
+        return []
 
 
 _READERS = {}
