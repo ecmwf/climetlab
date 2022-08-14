5706edccf1b45d57ae7f926cd9b974c6c694128c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2022-08-14 12:40:32 +0100

    fix

--- climetlab/wrappers/ndarray.py
+++ climetlab/wrappers/ndarray.py
@@ -18,20 +18,23 @@ class NumpyArrayWrapper(Wrapper):
     def plot_map(self, backend):
 
         wrapper = get_wrapper(backend.option("metadata"))
-        wrapper.plot_numpy(backend, self.data)
-        # metadata = metadata.field_metadata()
 
-        # backend.bounding_box(
-        #     north=metadata["north"],
-        #     south=metadata["south"],
-        #     west=metadata["west"],
-        #     east=metadata["east"],
-        # )
+        if hasattr(wrapper, "plot_numpy"):
+            return wrapper.plot_numpy(backend, self.data)
 
-        # backend.plot_numpy(
-        #     self.data.reshape(metadata.get("shape", self.data.shape)),
-        #     metadata=metadata,
-        # )
+        metadata = wrapper.field_metadata()
+
+        backend.bounding_box(
+            north=metadata["north"],
+            south=metadata["south"],
+            west=metadata["west"],
+            east=metadata["east"],
+        )
+
+        backend.plot_numpy(
+            self.data.reshape(metadata.get("shape", self.data.shape)),
+            metadata=metadata,
+        )
 
     def to_datetime_list(self):
         return [datetime.datetime.fromtimestamp(x * 1e-9) for x in self.data.tolist()]
