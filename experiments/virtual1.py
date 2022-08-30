f897c85a8294f77905368b11444e9cac9c9c2f39
Florian Pinault <Florian.Pinault@ecmwf.int>
2022-08-30 16:08:34 +0200

    dask and vagrant

--- experiments/virtual1.py
+++ experiments/virtual1.py
@@ -1,11 +1,32 @@
 import time
 
-from dask.distributed import Client, LocalCluster
+from dask.distributed import Client, LocalCluster, Worker, SSHCluster
 
 import climetlab as cml
 
+from climetlab.utils.dask import start
+
 cluster = LocalCluster(n_workers=10, processes=False)
 client = Client(cluster)
+
+#start(kind='local',cluster_kwargs=dict(n_workers=10, processes=False))
+
+#start('local-threads', n_workers=10)
+#start('local-processes', n_workers=10, threads_per_worker=2)
+
+import climetlab.debug
+start('ssh', cluster_kwargs= dict(
+    hosts = ['localhost','localhost','localhost', 'localhost'],
+    connect_options = [
+        {'port':5022, 'username':'vagrant', 'password':'vagrant'},
+        {'port':5023, 'username':'vagrant', 'password':'vagrant'},
+        {'port':5024, 'username':'vagrant', 'password':'vagrant'},
+        {'port':5025, 'username':'vagrant', 'password':'vagrant'},
+    ],
+    remote_python = '/usr/bin/python3',
+
+))
+#start('ssh', hosts = ['node1:23', 'node2:78'])
 
 
 ds = cml.load_source("virtual", param="msl")
