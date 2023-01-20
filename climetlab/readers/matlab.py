af3cd6376c329cddf48ec997b56803d9fd8617a1
Florian Pinault <Florian.Pinault@ecmwf.int>
2023-01-20 17:27:30 +0100

    added basic reader for matlab files

---
+++ climetlab/readers/matlab.py
@@ -0,0 +1,32 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from . import Reader
+
+
+class MatlabReader(Reader):
+    def __init__(self, source, path):
+        super().__init__(source, path)
+
+    def to_numpy(self, key=None, scipy_io_loadmat_kwargs={}):
+        from scipy.io import loadmat
+
+        ds = loadmat(self.path, **scipy_io_loadmat_kwargs)
+        if key is not None:
+            ds = ds[key]
+        return ds
+
+    def __iter__(self):
+        return iter([self])
+
+
+def reader(source, path, magic=None, deeper_check=False):
+
+    if magic[:7] == b"MATLAB ":
+        return MatlabReader(source, path)
---
+++ tests/readers/test_matlab_reader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import os
+
+import numpy as np
+
+import climetlab as cml
+
+
+def test_matlab_1():
+    here = os.path.dirname(__file__)
+    testfile = os.path.join(here, "little_endian.mat")
+    s = cml.load_source("file", testfile)
+    arr = s.to_numpy(key="floats")
+    ref = np.array([[2, 3], [3, 4]])
+    assert (arr == ref).all(), (arr, ref)
+
+
+if __name__ == "__main__":
+    from climetlab.testing import main
+
+    main(__file__)
