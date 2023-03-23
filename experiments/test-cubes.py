a544a518d98608e3a13e47d019aa30d9282cab24
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2023-03-23 16:57:52 +0000

    update

---
+++ experiments/test-cubes.py
@@ -0,0 +1,23 @@
+import climetlab as cml
+import climetlab.debug  # noqa
+
+t850 = cml.load_source("mars", param="t", level=850, grid=[1, 1], time=[1200, 1800])
+z500 = cml.load_source("mars", param="z", level=500, grid=[1, 1], time=[1200, 1800])
+
+ds = cml.load_source("multi", t850, z500)
+ds = ds.full("param", "levelist")
+print(len(ds))
+
+for i in range(len(ds)):
+    try:
+        print("---", ds[i])
+    except Exception as e:
+        print(e)
+
+############
+
+ds = cml.load_source("multi", t850, z500)
+c = ds.cube("time", "param_levelist")
+# print(c)
+print(c[0])
+print(c[1])
