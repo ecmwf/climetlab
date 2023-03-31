6d2d7d205fb69181cd5a34f78eb7fd2fcbc6883e
Florian Pinault <Florian.Pinault@ecmwf.int>
2023-03-31 14:08:59 +0200

    up

--- tests/documentation/test_notebooks.py
+++ tests/documentation/test_notebooks.py
@@ -11,14 +11,11 @@
 
 import os
 import re
+import sys
 
-# from climetlab.testing import IN_GITHUB, MISSING, climetlab_file
-from climetlab.testing import climetlab_file
+import pytest
 
-# import sys
-
-# import pytest
-
+from climetlab.testing import IN_GITHUB, MISSING, climetlab_file
 
 # See https://www.blog.pythonlibrary.org/2018/10/16/testing-jupyter-notebooks/
 
@@ -27,7 +24,18 @@ EXAMPLES = climetlab_file("docs", "examples")
 
 SKIP = (
     "13-icoads.ipynb",
-    # TODO re-enable notebook tests
+    # disable testing documentation because external download is failing
+    # https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.SP.list.v04r00.csv
+    # TODO: test separately the documentation.
+    "02-source-url.ipynb",
+    "03-source-cds.ipynb",
+    "04-source-mars.ipynb",
+    "05-source-open-data.ipynb",
+    "06-era5-temperature.ipynb",
+    "07-high-lows.ipynb",
+    "08-bufr-data.ipynb",
+    "09-mars-odb.ipynb",
+    "10-meteonet.ipynb",
     "11-weatherbench.ipynb",
     "12-hurricane-database.ipynb",
     "13-icoads.ipynb",
@@ -68,43 +76,40 @@ def notebooks_list():
     return sorted(notebooks)
 
 
-# disable testing documentation because external download is failing
-# https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.SP.list.v04r00.csv
-# TODO: test separately the documentation.
-# @pytest.mark.notebook
-# @pytest.mark.skipif(
-#     MISSING("nbformat", "nbconvert", "ipykernel"),
-#     reason="python package nbformat not installed",
-# )
-# @pytest.mark.skipif(not IN_GITHUB, reason="Not on GITHUB")
-# @pytest.mark.skipif(
-#     sys.platform == "win32", reason="Cannot execute notebooks on Windows"
-# )
-# @pytest.mark.parametrize("path", notebooks_list())
-# def test_notebook(path):
-#     import nbformat
-#     from nbconvert.preprocessors import ExecutePreprocessor
-#
-#     if path in SKIP:
-#         pytest.skip("Notebook marked as 'skip'")
-#
-#     if path in MARS:
-#         if not os.path.exists(os.path.expanduser("~/.ecmwfapirc")):
-#             pytest.skip("No ~/.ecmwfapirc")
-#
-#     if path in CDS:
-#         if not os.path.exists(os.path.expanduser("~/.cdsapirc")):
-#             pytest.skip("No ~/.cdsapirc")
-#
-#     # if path in TENSORFLOW:
-#     #     if sys.version_info >= (3, 9):
-#     #         pytest.skip("Tensorflow not yet ready on 3.9")
-#
-#     with open(os.path.join(EXAMPLES, path)) as f:
-#         nb = nbformat.read(f, as_version=4)
-#
-#     proc = ExecutePreprocessor(timeout=60 * 60 * 5, kernel_name="python3")
-#     proc.preprocess(nb, {"metadata": {"path": EXAMPLES}})
+@pytest.mark.notebook
+@pytest.mark.skipif(
+    MISSING("nbformat", "nbconvert", "ipykernel"),
+    reason="python package nbformat not installed",
+)
+@pytest.mark.skipif(not IN_GITHUB, reason="Not on GITHUB")
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Cannot execute notebooks on Windows"
+)
+@pytest.mark.parametrize("path", notebooks_list())
+def test_notebook(path):
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
+
+    if path in SKIP:
+        pytest.skip("Notebook marked as 'skip'")
+
+    if path in MARS:
+        if not os.path.exists(os.path.expanduser("~/.ecmwfapirc")):
+            pytest.skip("No ~/.ecmwfapirc")
+
+    if path in CDS:
+        if not os.path.exists(os.path.expanduser("~/.cdsapirc")):
+            pytest.skip("No ~/.cdsapirc")
+
+    # if path in TENSORFLOW:
+    #     if sys.version_info >= (3, 9):
+    #         pytest.skip("Tensorflow not yet ready on 3.9")
+
+    with open(os.path.join(EXAMPLES, path)) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=60 * 60 * 5, kernel_name="python3")
+    proc.preprocess(nb, {"metadata": {"path": EXAMPLES}})
 
 
 if __name__ == "__main__":
