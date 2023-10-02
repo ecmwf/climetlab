d41e35bc45319291a9e68f433eb612ca3c021ef5
Florian Pinault <Florian.Pinault@ecmwf.int>
2023-10-02 09:39:56 +0000

    added $hdates_from_date()

--- climetlab/utils/config.py
+++ climetlab/utils/config.py
@@ -180,6 +180,7 @@ def substitute(x, vars=None, ignore_missing=False):
     """Recursively substitute environment variables and dict values in a nested list ot dict of string.
     substitution is performed using the environment var (if UPPERCASE) or the input dictionary.
 
+
     >>> substitute({'bar': '$bar'}, {'bar': '43'})
     {'bar': '43'}
 
@@ -188,12 +189,18 @@ def substitute(x, vars=None, ignore_missing=False):
         ...
     KeyError: 'BAR'
 
-    >>> substitute({'bar': '$BAR'}, skip_missing=True)
+    >>> substitute({'bar': '$BAR'}, ignore_missing=True)
     {'bar': '$BAR'}
 
     >>> os.environ["BAR"] = "42"
     >>> substitute({'bar': '$BAR'})
     {'bar': '42'}
+
+    >>> substitute('$bar', {'bar': '43'})
+    '43'
+
+    >>> substitute('$hdates_from_date($date, 2015, 2018)', {'date': '2023-05-12'})
+    '2015-05-12/2016-05-12/2017-05-12/2018-05-12'
 
     """
     if vars is None:
@@ -211,28 +218,73 @@ def substitute(x, vars=None, ignore_missing=False):
             return x
 
         lst = []
-        for i, bit in enumerate(re.split(r"\$(\w+)", x)):
-            if i % 2:
+
+        for i, bit in enumerate(re.split(r"(\$(\w+)(\([^\)]*\))?)", x)):
+            i %= 4
+            if i in [2, 3]:
+                continue
+            if i == 1:
                 try:
-                    if bit.upper() == bit:
+                    if "(" in bit:
+                        pattern = r"\$(\w+)\(([^)]*)\)"
+                        match = re.match(pattern, bit)
+                        assert match, bit
+
+                        function_name = match.group(1)
+                        params = [p.strip() for p in match.group(2).split(",")]
+                        params = [
+                            substitute(p, vars, ignore_missing=ignore_missing)
+                            for p in params
+                        ]
+
+                        bit = dict(hdates_from_date=hdates_from_date)[function_name](
+                            *params
+                        )
+
+                    elif bit.upper() == bit:
                         # substitute by the var env if $UPPERCASE
-                        bit = os.environ[bit]
+                        bit = os.environ[bit[1:]]
                     else:
                         # substitute by the value in the 'vars' dict
-                        bit = vars[bit]
+                        bit = vars[bit[1:]]
                 except KeyError as e:
                     if not ignore_missing:
                         raise e
 
-            bit = substitute(bit, vars, ignore_missing=ignore_missing)
+            if bit != x:
+                bit = substitute(bit, vars, ignore_missing=ignore_missing)
 
             lst.append(bit)
-
-        lst = [e for e in lst if e != ""]
-
-        if len(lst) == 1:
-            return lst[0]
 
         return "".join(str(_) for _ in lst)
 
     return x
+
+
+def hdates_from_date(date, start_year, end_year):
+    """
+    Returns a list of dates in the format '%Y%m%d' between start_year and end_year (inclusive),
+    with the year of the input date.
+
+    Args:
+        date (str or datetime): The input date.
+        start_year (int): The start year.
+        end_year (int): The end year.
+
+    Returns:
+        List[str]: A list of dates in the format '%Y%m%d'.
+    """
+    if not str(start_year).isdigit():
+        raise ValueError(f"start_year must be an int: {start_year}")
+    if not str(end_year).isdigit():
+        raise ValueError(f"end_year must be an int: {end_year}")
+    start_year = int(start_year)
+    end_year = int(end_year)
+
+    from climetlab.utils.dates import to_datetime
+
+    date = to_datetime(date)
+    assert not (date.hour or date.minute or date.second), date
+
+    hdates = [date.replace(year=year) for year in range(start_year, end_year + 1)]
+    return "/".join(d.strftime("%Y-%m-%d") for d in hdates)
