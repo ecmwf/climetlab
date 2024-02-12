348561cc072a59e8cda14dca3e539d58c87aaa17
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-02-12 12:32:01 +0000

    hurdat database chnaged. no testing on it

--- docs/dataset-example.py
+++ docs/dataset-example.py
@@ -1,7 +1,7 @@
-import climetlab as cml
+# import climetlab as cml
 
-data = cml.load_dataset("hurricane-database", bassin="atlantic")
-print(data.home_page)
+# data = cml.load_dataset("hurricane-database", bassin="atlantic")
+# print(data.home_page)
 
-irma = data.to_pandas(name="irma", year=2017)
-cml.plot_map(irma)
+# irma = data.to_pandas(name="irma", year=2017)
+# cml.plot_map(irma)
